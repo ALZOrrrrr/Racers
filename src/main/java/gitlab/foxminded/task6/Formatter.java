a4854a03c4f220a5c931f2fb2ede76ae43e0feaa
Alexey <alzozp@gmail.com>
2020-03-12 22:14:06 +0200

    commit №7

--- src/main/java/gitlab/foxminded/task6/Formatter.java
+++ src/main/java/gitlab/foxminded/task6/Formatter.java
@@ -1,62 +1,45 @@
 package gitlab.foxminded.task6;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Formatter {
+    private int place;
 
     private String SPACE = " ";
 
+    private int getPlace(){
+        return place++;
+    }
     private void validate(List a) {
         if (a == null) {
             throw new IllegalArgumentException("should be list parameter");
         }
     }
-
     private String getBorder() {
         return ("------------------------------------------------------------------------\n");
     }
-
-    private String adddSpaces(int index) {
+    private String addSpaces(int index) {
         String returnString = "";
         for (int i = 0; i < index; i++) {
             returnString += SPACE;
         }
         return returnString;
     }
-
-    private String addRacers(List<Racer> list){
-        StringBuilder returnString = new StringBuilder();
-
-        for (int i = 0; i < list.size(); i++) {
-            int spaceIndex = 22;
-            if (list.get(i).getPlace() > 9) {
-                spaceIndex--;
-            }
-            returnString.append(list.get(i).getPlace() + "." + list.get(i).getName() + adddSpaces(spaceIndex - list.get(i).getName().length()) + "|" + SPACE + list.get(i).getCompany() + adddSpaces(30 - list.get(i).getCompany().length()) + "|" + SPACE);
-
-            long durationInMillis = list.get(i).getTime();
-            long millis = durationInMillis % 1000;
-            long second = (durationInMillis / 1000) % 60;
-            long minute = (durationInMillis / (1000 * 60)) % 60;
-
-            returnString.append(String.format("%02d:%02d.%d", minute, second, millis) + "\n");
-        }
-
-        return returnString.toString();
-
-
-
-
-
-    }
-
     public String format(List<Racer> list) {
+        place = 1;
         validate(list);
         StringBuilder returnString = new StringBuilder();
-         returnString.append(addRacers(list.stream().filter(x -> x.getPlace() < 16).collect(Collectors.toList())));
-         returnString.append(getBorder());
-        returnString.append(addRacers(list.stream().filter(x -> x.getPlace() >= 16).collect(Collectors.toList())));
+        list.stream().sorted().forEach(x -> {
+            if(x.getPlace()==16){
+            returnString.append(getBorder());
+            }
+            returnString.append(getPlace() + "." +
+                                x.getName() + addSpaces(22 - x.getName().length()) + "|" + SPACE +
+                                x.getCompany() + addSpaces(30 - x.getCompany().length()) + "|" + SPACE);
+                                returnString.append(String.format("%02d:%02d.%d", (x.getTime() / (1000 * 60)) % 60, (x.getTime() / 1000) % 60, x.getTime() % 1000) + "\n");
+        });
         return returnString.toString();
     }
 
--- src/main/java/gitlab/foxminded/task6/RacersTime.java
+++ src/main/java/gitlab/foxminded/task6/RacersTime.java
@@ -1,5 +1,6 @@
 package gitlab.foxminded.task6;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalTime;
@@ -7,29 +8,28 @@ import java.time.temporal.ChronoField;
 import java.util.*;
 import java.util.stream.Collectors;
 
+
     public class RacersTime {
-    private List<Racer> parseRacer(List<String[]> abbreviations, Map<String,LocalTime> startMap,Map<String,LocalTime> endMap){
-       return abbreviations.stream().map(x -> new Racer(x[1], x[2], endMap.get(x[0]).getLong(ChronoField.MILLI_OF_DAY) - startMap.get(x[0]).getLong(ChronoField.MILLI_OF_DAY))).collect(Collectors.toList());
-    }
+        private static Map<String, LocalTime> getRacesTime(String path){
+            Map<String, LocalTime> resultMap = new HashMap<>();
+            try {
+                Files.lines(Paths.get(path)).forEach(x -> resultMap.put(x.substring(0,3),LocalTime.parse(x.split("_")[1])));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return resultMap;
+        }
 
     public List<Racer> getRacers(String abbreviations, String startTime, String endTime)throws Exception {
 
         List<String[]> abbreviationsList = Files.lines(Paths.get(abbreviations)).map(x -> x.split("_")).collect(Collectors.toList());
 
-        Map<String, LocalTime> startMap = Files.lines(Paths.get(startTime)).collect(Collectors.toMap(x -> x.split("_")[0].substring(0,3), x -> LocalTime.parse(x.split("_")[1])));
+        Map<String, LocalTime> startMap = getRacesTime(startTime);
 
-        Map<String, LocalTime> endMap = Files.lines(Paths.get(endTime)).collect(Collectors.toMap(x -> x.split("_")[0].substring(0,3),x -> LocalTime.parse(x.split("_")[1])));
+        Map<String, LocalTime> endMap = getRacesTime(endTime);
 
-        List<Racer> returnList = parseRacer(abbreviationsList, startMap, endMap);
-
-        Collections.sort(returnList);
-
-        for (int i = 0; i < returnList.size(); i++) {
-            returnList.get(i).setPlace(i + 1);
-        }
-
-        return returnList;
+        return abbreviationsList.stream().map(x -> new Racer(x[1], x[2], endMap.get(x[0]).getLong(ChronoField.MILLI_OF_DAY) - startMap.get(x[0])
+                .getLong(ChronoField.MILLI_OF_DAY)))
+                .collect(Collectors.toList());
     }
-
-
 }
