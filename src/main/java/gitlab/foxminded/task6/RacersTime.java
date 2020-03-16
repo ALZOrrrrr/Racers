b8bd714fa7170e32ddee607ca7fc77bca318ba4a
Alexey <alzozp@gmail.com>
2020-03-16 18:44:37 +0200

    commit №10

--- src/main/java/gitlab/foxminded/task6/RacersTime.java
+++ src/main/java/gitlab/foxminded/task6/RacersTime.java
@@ -9,28 +9,43 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 
+
+
+
+
     public class RacersTime {
-        private static Map<String, LocalTime> getRacesTime(String path){
+
+
+        private void validate(String a, String b, String c) {
+            if (a == null || b == null || c == null)
+                throw new IllegalArgumentException("null transferred");
+        }
+
+        private static Map<String, LocalTime> getRacesTime(String path) {
             Map<String, LocalTime> resultMap = new HashMap<>();
             try {
                 Files.lines(Paths.get(path))
-                        .forEach(x -> resultMap.put(x.substring(0,3),LocalTime.parse(x.split("_")[1])));
+                        .forEach(x -> resultMap.put(x.substring(0, 3), LocalTime.parse(x.split("_")[1])));
             } catch (IOException e) {
                 System.out.println("File has to be text format");
             }
             return resultMap;
         }
 
-    public List<Racer> getRacers(String abbreviations, String startTime, String endTime)throws Exception {
+        public List<Racer> getRacers(String abbreviations, String startTime, String endTime){
+            validate(abbreviations,startTime,endTime);
+            try {
+                Map<String, LocalTime> startMap = getRacesTime(startTime);
+                Map<String, LocalTime> endMap = getRacesTime(endTime);
 
-        Map<String, LocalTime> startMap = getRacesTime(startTime);
-
-        Map<String, LocalTime> endMap = getRacesTime(endTime);
-
-        return Files.lines(Paths.get(abbreviations))
-                .map(x -> x.split("_"))
-                .map(x -> new Racer(x[1], x[2], endMap.get(x[0]).getLong(ChronoField.MILLI_OF_DAY) - startMap.get(x[0])
-                .getLong(ChronoField.MILLI_OF_DAY)))
-                .collect(Collectors.toList());
+                return Files.lines(Paths.get(abbreviations))
+                        .map(x -> x.split("_"))
+                        .map(x -> new Racer(x[1], x[2], endMap.get(x[0]).getLong(ChronoField.MILLI_OF_DAY) - startMap.get(x[0])
+                                .getLong(ChronoField.MILLI_OF_DAY)))
+                        .collect(Collectors.toList());
+            } catch (Exception e) {
+                System.out.println("files not found");
+                return Collections.emptyList();
+            }
+        }
     }
-}
--- src/test/java/RacersTimeTest.java
+++ src/test/java/RacersTimeTest.java
@@ -2,16 +2,32 @@ import gitlab.foxminded.task6.Racer;
 import gitlab.foxminded.task6.RacersTime;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RacersTimeTest {
     private Racer expectedStandart = new Racer();
     private RacersTime racersTime = new RacersTime();
     @Test
-    public void standartTest() throws Exception{
+    public void standartTest() {
         List<Racer> actual = racersTime.getRacers("abbreviations.txt", "start.log", "end.log");
         assertEquals(expectedStandart, actual.get(0));
     }
+    @Test
+    public void shouldThrowIllegalArgumentExceptionWhenNullsPassed() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            racersTime.getRacers(null, null, null);
+        });
+        final String expectedMessage = "null transferred";
+        final String actualMessage = exception.getMessage();
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+    @Test
+    public void fileNotFoundTest() {
+        List actual = racersTime.getRacers("a", "start.log", "end.log");
+        assertEquals(Collections.emptyList(), actual);
+    }
 }
+
